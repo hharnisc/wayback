a9ada8154e48a9ee0a12e8e5141b593f80fd1374
Harrison Harnisch <hharnisc@gmail.com>
2015-07-09 21:04:48 -0700

    implement push op

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
+import * as hat from "hat";
+
 const model = Symbol("model");
+const rack = Symbol("rack");
 
 export class Wayback {
   constructor() {
     this[model] = {};
+    this[rack] = hat.rack();
   }
 
   model() {
     return this[model];
+  }
+
+  push(data) {
+    let id = this[rack]();
+    this[model][id] = data;
+    return id;
   }
 }
--- test/test.js
+++ test/test.js
@@ -18,4 +18,16 @@ describe("wayback tests", () => {
   it("does create empty wayback", () => {
     expect(wayback.model()).to.eql({});
   });
+
+  it("does push new data onto wayback", () => {
+    // push new data onto wayback
+    let data = {message: "sup"};
+    let id = wayback.push(data);
+
+    // make sure there is one revision
+    let model = wayback.model();
+    let modelKeys = Object.keys(model);
+    expect(modelKeys.length).to.equal(1);
+    expect(model[id]).to.equal(data);
+  });
 });
