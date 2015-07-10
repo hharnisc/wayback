1396de2af0fba47f5948c5eb1fb44c0d04c88a47
Harrison Harnisch <hharnisc@gmail.com>
2015-07-09 19:23:26 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Wayback
+Manage sequences of arbitrary data with revisions
+
+
+## Run
+    npm install
+    npm start
+
+
+## Test
+    npm test
+    npm run tdd  # (Watch)
---
+++ test/test.js
@@ -0,0 +1,16 @@
+/*global describe*/
+/*global beforeEach*/
+/*global afterEach*/
+/*global before*/
+/*global after*/
+/*global it*/
+
+"use strict";
+
+import { expect } from "chai";
+
+describe("wayback tests", () => {
+  it("succeeds", () => {
+    expect(123).to.equal(123);
+  });
+});
